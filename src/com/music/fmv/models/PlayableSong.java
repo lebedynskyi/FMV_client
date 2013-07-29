3eca013745f2e44a65fdb7f988eaa18797ee69e4
vitaliylebedinskiy <789asd789>
2013-07-29 17:38:07 +0300

    Implemented body of PlayAble class

--- src/com/music/fmv/models/PlayableSong.java
+++ src/com/music/fmv/models/PlayableSong.java
@@ -6,5 +6,40 @@ package com.music.fmv.models;
  * Time: 4:04 PM
  */
 public class PlayableSong extends BaseSerializableModel{
+    private String url;
+    private String artist;
+    private String title;
+    private int duration;
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
